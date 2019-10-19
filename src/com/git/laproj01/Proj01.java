ad019511a8a2bedc948781808d621743136c8f85
Abhijit Samantaray <Abhijit Samantaray@LAPTOP-IS27TH9F>
2019-10-19 23:07:57 +0530

    First commit. Please be checked ,report if modification is required.

---
+++ src/com/git/laproj01/Proj01.java
@@ -0,0 +1,12 @@
+package com.git.laproj01;
+
+public class Proj01 {
+
+	public static void main(String[] args) {
+		System.out.println("Data has been recieved.");
+		System.out.println("Proj01.main()");
+		System.out.println("Please be remind......");
+
+	}
+
+}
